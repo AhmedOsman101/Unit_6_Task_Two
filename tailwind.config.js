f9cfc5b27b073620369394a05f5c8799af9304b4
AhmadOthman <trollingosman101@outlook.com>
2024-05-01 20:12:33 +0300

    manually enabled dark mode in the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-    "./src/**/*.{js,jsx,ts,tsx}", 
-    "./index.html"
-  ],
+	darkMode: "selector",
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
