189e808e4f81f5a927ccd794c37d008cbd4036f5
AhmadOthman <trollingosman101@outlook.com>
2024-05-01 20:53:57 +0300

    update 1 file and create 1 file

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Error404 from "../components/404.jsx";
+import Home from "./Home.jsx";
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<div>App</div>} />
+				<Route path="/" Component={Home} />
+				<Route path="/about" Component={Home} />
+				<Route path="/contact" Component={Home} />
 				<Route path="/*" Component={Error404} />
 			</Routes>
 		</>
