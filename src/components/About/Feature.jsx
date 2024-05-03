e22b4dcfee7075599ff7c2b17df57a3afb0400ed
AhmadOthman <trollingosman101@outlook.com>
2024-05-03 22:06:50 +0300

    update 6 files and create 1 file

---
+++ src/components/About/Feature.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+/* eslint-disable react/prop-types */
+const Feature = ({ feature }) => {
+	useEffect(() => {
+		AOS.init();
+	}, []);
+	return (
+		<>
+			<div
+				data-aos-duration="1000"
+				data-aos-easing="ease-in-out"
+				data-aos={feature.id % 2 == 0 ? "fade-right" : "fade-left"}
+				className={
+					"flex flex-col overflow-hidden rounded-md shadow-sm items-stretch " +
+					(feature.id % 2 == 0
+						? "lg:flex-row"
+						: "lg:flex-row-reverse")
+				}>
+				<img
+					src={feature.image}
+					className="h-80 dark:bg-gray-500 aspect-video"
+				/>
+				<div className="flex flex-col justify-center flex-1 p-5 dark:bg-gray-800">
+					<h3 className="text-3xl font-bold">{feature.name}</h3>
+					{feature &&
+						feature.content.map((item, index) => {
+							return (
+								<p
+									className={
+										"dark:text-gray-400 " +
+										(index + 1 == feature.content.length
+											? "my-3"
+											: "mt-3")
+									}
+									key={index}>
+									{item}
+								</p>
+							);
+						})}
+					<Link
+						to="/#plans"
+						className="relative px-6 py-3 overflow-hidden font-semibold rounded dark:bg-gray-100 dark:text-gray-900 self-start">
+						view available plans
+						<span className="absolute top-0 right-0 px-5 py-1 text-[0.65rem] tracking-wider text-center uppercase whitespace-no-wrap origin-bottom-left transform rotate-45 -translate-y-full translate-x-1/3 dark:bg-cyan-400">
+							Hot
+						</span>
+					</Link>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Feature;
--- src/components/About/features.jsx
+++ src/components/About/features.jsx
@@ -2,13 +2,16 @@
 /* eslint-disable react/no-unescaped-entities */
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Feature from "./Feature";
 
 const AboutFeatures = () => {
 	const [aboutFeatures, setAboutFeatures] = useState([]);
 	const fetchData = async () => {
-		const data = (await axios.get("http://localhost:3000/FAQs")).data;
+		const data = (await axios.get("http://localhost:3000/Features")).data;
+		console.log(data);
 		return data;
 	};
+	// lg:flex-row-reverse ? lg:flex-row
 	useEffect(() => {
 		fetchData().then((data) => {
 			setAboutFeatures(data);
@@ -18,74 +21,10 @@ const AboutFeatures = () => {
 		<>
 			<section className="p-4 lg:p-8 dark:bg-gray-900 dark:text-gray-100">
 				<div className="container mx-auto space-y-12">
-					<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-						<img
-							src="https://source.unsplash.com/640x480/?1"
-							className="h-80 dark:bg-gray-500 aspect-video"
-						/>
-						<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-800">
-							<span className="text-xs uppercase dark:text-gray-400">
-								Join, it's free
-							</span>
-							<h3 className="text-3xl font-bold">
-								We're not reinventing the wheel
-							</h3>
-							<p className="my-6 dark:text-gray-400">
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Dolor aliquam possimus quas,
-								error esse quos.
-							</p>
-							<button type="button" className="self-start">
-								Action
-							</button>
-						</div>
-					</div>
-					<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
-						<img
-							src="https://source.unsplash.com/640x480/?2"
-							alt=""
-							className="h-80 dark:bg-gray-500 aspect-video"
-						/>
-						<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-800">
-							<span className="text-xs uppercase dark:text-gray-400">
-								Join, it's free
-							</span>
-							<h3 className="text-3xl font-bold">
-								We're not reinventing the wheel
-							</h3>
-							<p className="my-6 dark:text-gray-400">
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Dolor aliquam possimus quas,
-								error esse quos.
-							</p>
-							<button type="button" className="self-start">
-								Action
-							</button>
-						</div>
-					</div>
-					<div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-						<img
-							src="https://source.unsplash.com/640x480/?3"
-							alt=""
-							className="h-80 dark:bg-gray-500 aspect-video"
-						/>
-						<div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-800">
-							<span className="text-xs uppercase dark:text-gray-400">
-								Join, it's free
-							</span>
-							<h3 className="text-3xl font-bold">
-								We're not reinventing the wheel
-							</h3>
-							<p className="my-6 dark:text-gray-400">
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Dolor aliquam possimus quas,
-								error esse quos.
-							</p>
-							<button type="button" className="self-start">
-								Action
-							</button>
-						</div>
-					</div>
+					{aboutFeatures &&
+						aboutFeatures.map((feature, index) => {
+							return <Feature key={index} feature={feature} />;
+						})}
 				</div>
 			</section>
 		</>
