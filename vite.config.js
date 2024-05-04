e46d6aa2d05006200059ebc861faeb8dba9a259f
AhmadOthman <trollingosman101@outlook.com>
2024-05-04 21:21:09 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "Unit_6_Task_Two",
+	base: "/Unit_6_Task_Two",
 	plugins: [react()],
 });
