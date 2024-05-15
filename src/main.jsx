42aac4a4d58be4a3bc6834a31a1e2055cd669d62
AhmadOthman <trollingosman101@outlook.com>
2024-05-15 14:30:45 +0300

    update src/css/index.css and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./Pages/App.jsx";
 import "./css/index.css";
 import "aos/dist/aos.css";
-import { BrowserRouter } from "react-router-dom";
+
+// document.designMode = "on";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<BrowserRouter basename="Unit_6_Task_Two">
